245f2b2587e0dc9dab008046f3b871822a24bf87
joshua-miles <joshua@christetulit.org>
2019-08-08 16:24:58 -0500

    -

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Counter from './Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    count: null
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3001/counts/1')
+      .then( res => res.json())
+      .then( (result) => {
+        this.setState({ 
+          count: result.count
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        { 
+          this.state.count != null 
+            ?
+          <Counter count={this.state.count}/>
+            :
+          null
+        } 
+      </div>
+    );
+  }
+
 }
 
 export default App;
